3b855a5cd5566baff5872e7d38e7c4ed7d1c2773
Aneesh Puliyedath Udumbath <aneesh.udumbath@RACKSPACE.COM>
2014-03-04 10:16:13 +0530

    Working version

---
+++ hello.py
@@ -0,0 +1,22 @@
+from wsgiref.simple_server import make_server
+
+def app(env, start_response):
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/plain')]
+
+    start_response(status, response_headers)
+
+    response_body = "Hello World"
+    return [response_body]
+
+
+class AllCaps:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, env, start_response):
+        return self.app(env, start_response)
+
+all_caps_middleware = AllCaps(app)
+httpd = make_server("localhost", 8000, all_caps_middleware)
+httpd.serve_forever()
