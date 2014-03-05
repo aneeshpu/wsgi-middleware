1e4188e92783e36ac1e02b73f41ed82f7b4f89ca
Aneesh Puliyedath Udumbath <aneesh.udumbath@RACKSPACE.COM>
2014-03-05 06:26:49 +0530

    Removed redundant code

--- hello.py
+++ hello.py
@@ -2,39 +2,9 @@ from wsgiref.simple_server import make_server
 from webob.dec import wsgify
 import webob
 
-def makeapp(f):
-    def wrapped_func(env, start_response):
-        status = '200 OK'
-        response_headers = [('Content-Type', 'text/plain'), ('random-nonsense', 'yeah works')]
-        start_response(status, response_headers)
-
-        return f()
-
-    return wrapped_func
-
-
 @wsgify
 def func(req):
     return webob.Response('wsgified application')
-
-def app(env, start_response):
-    status = '200 OK'
-    response_headers = [('Content-Type', 'text/plain')]
-
-    start_response(status, response_headers)
-
-    response_body = "Hello World"
-    return [response_body]
-
-
-class AllCaps:
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, env, start_response):
-        resp =  self.app(env, start_response)
-        resp = [r.upper() for r in resp]
-        return resp
 
 httpd = make_server("localhost", 8000, func)
 httpd.serve_forever()
