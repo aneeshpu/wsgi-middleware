6efdd2086829ea7f1e05d3a7e72addf407a3d37a
Aneesh Puliyedath Udumbath <aneesh.udumbath@RACKSPACE.COM>
2014-03-05 06:38:49 +0530

    Implemented a middleware

--- hello.py
+++ hello.py
@@ -2,9 +2,21 @@ from wsgiref.simple_server import make_server
 from webob.dec import wsgify
 import webob
 
+@wsgify.middleware
+def my_dec(req, app):
+    print "inside my_dec"
+    print app
+    resp = req.get_response(app)
+    resp.body = resp.body.upper()
+    return resp
+
+    
+
 @wsgify
 def func(req):
+    print "inside func"
     return webob.Response('wsgified application')
 
-httpd = make_server("localhost", 8000, func)
+middleware = my_dec(func)
+httpd = make_server("localhost", 8000, middleware)
 httpd.serve_forever()
